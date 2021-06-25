028f000c84351f9e072ae98b2849c4a74dcb4978
keilaviana <keilavg72@gmail.com>
2021-06-25 18:59:07 -0300

    adicionando quem é responsável pela view engine

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const route = require('./route')
+const path = require('path')
 
 // executando o express
 const server = express()
+
+// especificando quem é responsável pela view engine é o módulo ejs
+server.set('view engine', 'ejs')
+
+// o path pega o caminho da pasta que está o projeto 
+server.set('views', path.join(__dirname, 'views'))
 
 server.use(route)
 

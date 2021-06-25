c3b416adef96e09b0f830b2711550031d2d6b647
keilaviana <keilavg72@gmail.com>
2021-06-25 18:48:30 -0300

    chamando o script de rotas dentro do server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const route = require('./route')
 
 // executando o express
 const server = express()
+
+server.use(route)
 
 //setando qual porta que a aplicação vai rodar
 server.listen(5000, () => console.log('rodando'))

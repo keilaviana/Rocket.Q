6724e33607caaa4b372a7972160a97fcf85f0a4b
keilaviana <keilavg72@gmail.com>
2021-06-26 12:47:50 -0300

    Mudando o texto do modal de acordo com o botão selecionado

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,21 +2,27 @@ import Modal from './modal.js'
 
 const modal = Modal()
 
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
 //Pega todos os botões de classe check
 const checkButtons = document.querySelectorAll(".actions a.check")
 
 checkButtons.forEach(button =>{
-    button.addEventListener("click", event =>{
-        //Abre o modal chamando o arquivo modal.js
-        modal.open() 
-    })
+    button.addEventListener("click", handleClick)
 })
 
 const deleteButton = document.querySelectorAll(".actions a.delete")
 
 deleteButton.forEach(button => {
-    button.addEventListener("click", event =>{
-        modal.open()
-    })
+    button.addEventListener("click",(event) => handleClick(event, false))
 })
 
+function handleClick(event, check = true){
+    modalTitle.innerHTML = check? "Marcar como lida esta pergunta" : "Excluir esta pergunta"
+    modalDescription.innerHTML = check? "Tem certeza que deseja marcar como lida esta pergunta?" : "Tem certeza que deseja excluir esta pergunta?"
+
+    //abrir modal
+    modal.open()
+}

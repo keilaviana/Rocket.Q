9e90daa9ee286dbdc502547206878eb344978b09
keilaviana <keilavg72@gmail.com>
2021-06-26 15:54:06 -0300

    Criando template com o ejs na pagina index e create-pass

--- src/route.js
+++ src/route.js
@@ -5,9 +5,10 @@ const QuestionController = require('./controllers/QuestionController')
 const route = express.Router()
 
 // requisição e resposta da rota
-route.get('/', (req, res) => res.render("index"))
 route.get('/room', (req,res) => res.render("room"))
-route.get('/create-pass', (req, res) => res.render("create-pass"))
+route.get('/', (req, res) => res.render("index", {page: 'enter-room'} ))
+route.get('/create-pass', (req, res) => res.render("index", {page: "create-pass"}))
+
 
 
 // Formato que o formulário de dentro da modal tem que passar a informação

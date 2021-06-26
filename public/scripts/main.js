ccd71fcaa98ab32a83b4f9ccbe7a1c7e926ccce7
keilaviana <keilavg72@gmail.com>
2021-06-26 12:22:54 -0300

    adicionando função de abrir modal no botão de excluir pergunta

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -12,3 +12,11 @@ checkButtons.forEach(button =>{
     })
 })
 
+const deleteButton = document.querySelectorAll(".actions a.delete")
+
+deleteButton.forEach(button => {
+    button.addEventListener("click", event =>{
+        modal.open()
+    })
+})
+

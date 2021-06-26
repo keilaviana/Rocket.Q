ca63c985bc8d80f7059329d181ee2d8b70503948
keilaviana <keilavg72@gmail.com>
2021-06-26 12:08:52 -0300

    adicionando função p fechar modal

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,5 +1,8 @@
 export default function Modal(){
 
+    const canceButton = document.querySelector('.button.cancel')
+    canceButton.addEventListener("click", close)
+
     function open(){
         document.querySelector('.modal-wrapper').classList.add("active")
     }

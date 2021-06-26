e6c0ab1951673585a4d78443841e9eb9da00bf7e
keilaviana <keilavg72@gmail.com>
2021-06-26 16:50:26 -0300

    ajustando rotas para pegar id da sala

--- src/route.js
+++ src/route.js
@@ -1,17 +1,19 @@
 const express = require('express')
 const QuestionController = require('./controllers/QuestionController')
+const RoomController = require('./controllers/RoomController')
 
 //guarda as funcionalidades de rotas
 const route = express.Router()
 
 // requisição e resposta da rota
-route.get('/room', (req,res) => res.render("room"))
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'} ))
+route.get('/room/:room', (req,res) => res.render("room"))
 route.get('/create-pass', (req, res) => res.render("index", {page: "create-pass"}))
 
 
 
 // Formato que o formulário de dentro da modal tem que passar a informação
-route.post('/room/:room/:question/:action', QuestionController.index )
+route.post('/question/:room/:question/:action', QuestionController.index )
+route.post('/create-room', RoomController.create)
 
 module.exports = route

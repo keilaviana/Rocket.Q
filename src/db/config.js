72ed083677cda2dd84bf6221b5d016917ae482db
keilaviana <keilavg72@gmail.com>
2021-06-26 16:57:45 -0300

    adicionando configuração do banco de dados

---
+++ src/db/config.js
@@ -0,0 +1,9 @@
+const sqlite3 = require("sqlite3")
+const {open} = require("sqlite") // trás apenas o open ao inves de trazer tudo que tem dentro do sqlite
+
+module.exports = () => {
+    open({
+        filename: '/src/db/rocketq.sqlite',
+        driver: sqlite3.Database
+    })
+}

fa0356c975b280f9753fb283e878f34c3554591c
keilaviana <keilavg72@gmail.com>
2021-06-27 15:47:35 -0300

    Criando id dinâmico para as salas

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -6,20 +6,33 @@ module.exports = {
         const pass = req.body.password
 
         let roomId
+        let isRoom = true
 
-        for(var i = 0; i <6; i++){
-            i == 0? roomId = Math.floor(Math.random()*10).toString() :
-            roomId += Math.floor(Math.random()*10).toString()
+        while (isRoom) {
+            // Gera o numero da sala
+            for(var i = 0; i <6; i++){
+                i == 0? roomId = Math.floor(Math.random()*10).toString() :
+                roomId += Math.floor(Math.random()*10).toString()
+            }
+
+            // verificar se o numero ja existe
+            const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+        
+            isRoom = roomsExistIds.some(roomsExistId => roomsExistId === roomId)
+
+            if(! isRoom){
+        
+                // Inseri a sala no banco
+                        await db.run(`INSERT INTO rooms (
+                            id,
+                            pass
+                        )VALUES (
+                            ${parseInt(roomId)},
+                            ${pass}
+                        )`)
+            }
         }
-
-
-        await db.run(`INSERT INTO rooms (
-            id,
-            pass
-        )VALUES (
-            ${parseInt(roomId)},
-            ${pass}
-        )`)
+        
 
         await db.close()
 

eb7f76aa8bfd1dc476e011eedd0d8429289afa6a
keilaviana <keilavg72@gmail.com>
2021-06-27 16:35:12 -0300

    cadastrando perguntas no banco de dados

--- src/route.js
+++ src/route.js
@@ -7,13 +7,14 @@ const route = express.Router()
 
 // requisição e resposta da rota
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'} ))
-route.get('/room/:room', (req,res) => res.render("room"))
 route.get('/create-pass', (req, res) => res.render("index", {page: "create-pass"}))
 
+route.post('/create-room', RoomController.create)
+route.get('/room/:room', RoomController.open)
 
 
+route.post('/question/create/:room', QuestionController.create)
 // Formato que o formulário de dentro da modal tem que passar a informação
 route.post('/question/:room/:question/:action', QuestionController.index )
-route.post('/create-room', RoomController.create)
 
 module.exports = route
